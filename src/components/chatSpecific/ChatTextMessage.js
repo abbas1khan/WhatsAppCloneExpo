bb04893691aae872181832b4acb755bce58fb035
Abbas Khan <khanabbas161@gmail.com>
2024-04-21 01:32:24 +0530

    gg

--- src/components/chatSpecific/ChatTextMessage.js
+++ src/components/chatSpecific/ChatTextMessage.js
@@ -6,12 +6,12 @@ import DoubleBlueTickSVG from '../../assets/SVG_Components/DoubleBlueTickSVG'
 
 const chatTextMessage = ({ item }) => {
     return (
-        <View style={{ maxWidth: sizes.width * 0.8, paddingVertical: 5, paddingHorizontal: 10, borderRadius: 12, backgroundColor: colors.messageBackground, }}>
+        <View style={{ maxWidth: sizes.width * 0.8, paddingVertical: 5, paddingBottom: 3, paddingHorizontal: 10, borderRadius: 12, backgroundColor: colors.messageBackground, }}>
             <Text style={{ fontSize: 16, color: colors.white }}>
                 {item?.content}
             </Text>
             <View style={{ flexDirection: 'row', alignSelf: 'flex-end', alignItems: 'flex-end', }}>
-                <Text style={{ fontSize: 11, marginTop: 6, marginRight: 4, color: colors.messageTime }}>
+                <Text style={{ fontSize: 12, marginTop: 5, marginRight: 4, color: colors.messageTime }}>
                     {moment(item?.createdAt).format('h:mm a')}
                 </Text>
 
